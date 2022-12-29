55fffa19d12dd0ffb7e45e7f9c1cd2ea980fecf1
Claire Lin <chunhuilin7516@gmail.com>
2022-12-29 17:23:04 +0800

    link emailJS with contact sector

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,47 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactMessage = document.getElementById('contact-message'),
+      contactNotice = document.getElementById('contact-notice')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+  // check if the field has a value
+  if(contactName.value === '' || contactEmail.value === '' || contactMessage.value === ''){
+    // Add and remove color
+    contactNotice.classList.remove('color-blue')
+    contactNotice.classList.add('color-red')
+
+    // Show input field notice
+    contactNotice.textContent = 'Write all the input fields 💁🏻‍♀️'
+  }else{
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_qbdqjjw','template_btooqrm','#contact-form','9TX9W8mblwnh2ERa7')
+        .then(() =>{
+          // show message and add color
+          contactNotice.classList.add('color-blue')
+          contactNotice.textContent = 'Message sent ✅'
+
+          // Remove message after five seconds
+          setTimeout(() =>{
+            contactNotice.textContent = ''
+          }, 5000)
+        }, (error) =>{
+          alert('OOPS! SOMETHING HAS FAILED...', error)
+        })
+
+      // To clear the input field
+      contactName.value = ''
+      contactEmail.value = ''
+      contactMessage.value = ''
+  }
+}
+contactForm.addEventListener('submit', sendEmail)
+
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
