946ddcde05c9a4fcdffaaebf5ae31710075c9212
Claire Lin <chunhuilin7516@gmail.com>
2023-01-06 19:18:18 +0800

    finish responsive design for medium devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,7 +186,7 @@ const sr = ScrollReveal({
   // reset: true /* Animations repeat */
 })
 
-sr.reveal(`.home__data, .projects__container, .testimonial__container, .footer__container`)
+sr.reveal(`.home__data, .about__description, .projects__container, .testimonial__container, .footer__container`)
 sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
 sr.reveal(`.skills__content:nth-child(1)`, {origin: 'bottom'})
 sr.reveal(`.skills__content:nth-child(2)`, {origin: 'bottom'})
